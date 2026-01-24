e46c25cfe777605565a0f3c9b3e73ad411e458a2
sloht <sohammediwala@gmail.com>
2026-01-24 17:01:30 -0600

    relative functions

--- src/autons/wingrush.cpp
+++ src/autons/wingrush.cpp
@@ -7,6 +7,17 @@
 #include "auton.hpp"
 #endif
 
+void move_relative(double x, double y, double timeout, bool forwards, float speed=127){
+    chassis.moveToPoint(chassis.getPose().x + x,
+                              chassis.getPose().y + y,
+                              timeout, {forwards, speed});
+}
+void drive_distance(double distance, double timeout, float speed=127){
+    chassis.moveToPoint(chassis.getPose().x + distance * cos((-chassis.getPose().theta+90) * M_PI / 180.0),
+                              chassis.getPose().y + distance * sin((-chassis.getPose().theta+90) * M_PI / 180.0),
+                              timeout, {(distance>=0), speed});
+} 
+
 const void wingrush::left() {
     leftMG.move(127);
     rightMG.move(127);

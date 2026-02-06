3ea574b4822e75e6ed5ffc0740ff4b507412cfe2
sloht <sohammediwala@gmail.com>
2026-02-06 10:22:16 -0600

    tmp

--- src/autons/wingrush.cpp
+++ src/autons/wingrush.cpp
@@ -72,14 +72,15 @@ const void wingrush::right() {
     chassis.turnToHeading(90, 5000);
     chassis.moveToPoint(61.748+4.7, 45.8-2.5, 2000);
     chassis.waitUntilDone();
-    pros::delay(5);
-    chassis.moveToPoint(29.021, 46.661-5.5, 1900, {.forwards = false});
+    pros::delay(120);
+    chassis.moveToPoint(29.021, 46.661-5.5, 1600, {.forwards = false});
     chassis.waitUntilDone();
     outtake.move(127);
     chassis.waitUntilDone();
     descore.retract();
     chassis.setPose(29.021, 47.479, 90);
     pros::delay(2000);
-    chassis.moveToPoint(27.793, 38.888, 3000, {.forwards = true});
-    chassis.moveToPose(1, 41.138, 90, 9123, {.forwards = false});
+    chassis.moveToPoint(36.18, 38.843+1, 3000, {.forwards = true});
+    chassis.turnToHeading(95,9000);
+    chassis.moveToPoint(5.907, 41.138, 5000, {.forwards = false});
 }
